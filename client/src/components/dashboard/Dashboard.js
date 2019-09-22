fee83ac8b2ff95043725a8848bb465dac0e4edbe
Shahbaz Alam <shahbaz.alam@hashedin.com>
2019-09-23 03:06:06 +0530

    First Commit

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,43 @@
+import React, { useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import Spinner from '../layout/spinner';
+import { getCurrentProfile } from '../../actions/profile';
+import  DashboardActions  from './DashboardActions'
+
+
+
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
+    useEffect(() => {
+        getCurrentProfile();
+    }, []);
+
+    return loading && profile === null ? <Spinner /> : <Fragment>
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+            <i className="fas fa-user"></i> Welcome { user && user.name }
+        </p>
+        { profile !== null ? (<Fragment><DashboardActions /></Fragment>): (
+            <Fragment>
+                <p>You do not have a profile. Please create one.</p>
+                <Link to="/create-profile" className="btn btn-primary my-1">
+                    Create Profile
+                </Link>
+            </Fragment>
+            ) }
+    </Fragment>
+}
+
+Dashboard.propTypes = {
+    getCurrentProfle: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, { getCurrentProfile})(Dashboard)
